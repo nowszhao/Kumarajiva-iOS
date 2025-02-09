8f82e82e3b34f9ea0c06d9eafc1292a6c440c40a
nowszhao <nowszhao@gmail.com>
2025-02-09 17:54:43 +0800

    update

---
+++ Kumarajiva-iOS/Kumarajiva_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kumarajiva_iOSApp.swift
+//  Kumarajiva-iOS
+//
+//  Created by changhozhao on 2025/2/9.
+//
+
+import SwiftUI
+
+@main
+struct Kumarajiva_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
